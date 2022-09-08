4a21687b427d3854c64ca491f50c8d37a9065318
pixel-Teee <1173769234@qq.com>
2022-09-09 00:30:21 +0800

    begin process argument command line

--- Core/main.cpp
+++ Core/main.cpp
@@ -1,62 +1,17 @@
 #include "PreCompiled.h"
-/*
-std::ostream& operator<<(std::ostream& stream, const CXString& str)
-{
-	stream << clang_getCString(str);
-	clang_disposeString(str);
-	return stream;
-}
-*/
 
 int main(int argc, char* argv[])
 {
-	/*
-	kainjow::mustache::mustache tmpl("Hello {{what}}!");
-
-	std::cout << tmpl.render({ "what", "World" }) << std::endl;
-	*/
-
-	//CXIndex index = clang_createIndex(0, true);
-
-	//auto& path = std::filesystem::current_path();
-
-	//std::cout << path.string() << std::endl;
-
-	/*
-	std::vector<const char*> arguments = {
-		{"-x"},
-		{"c++"},
-		{"-std=c++11"}
-	};
-	
-
-	CXTranslationUnit unit = clang_parseTranslationUnit(index, "Debug//test//Test.h", arguments.data(), static_cast<int>(arguments.size()), nullptr, 0, CXTranslationUnit_None);
-
-	if (unit == nullptr)
-	{
-		std::cerr << "unable to parse translation unit" << std::endl;
-		exit(1);
-	}
-
-	CXCursor cusor = clang_getTranslationUnitCursor(unit);
-	clang_visitChildren(cusor, [](CXCursor c, CXCursor parent, CXClientData clientData)
-	{
-		//std::cout << "Cusor kind: " << clang_getCursorKind(c) << std::endl;
-		//return CXChildVisit_Recurse;
-
-		std::cout << "Cursor '" << clang_getCursorSpelling(c) << "' of kind '"
-			<< clang_getCursorKindSpelling(clang_getCursorKind(c)) << "'\n";
-		return CXChildVisit_Recurse;
-	}, nullptr);
-
-	clang_disposeTranslationUnit(unit);
-	clang_disposeIndex(index);
-	*/
-
 	auto start = std::chrono::system_clock::now();
 
 	//------from the command argument line to get file name------
+	//std::cout << argv[0] << std::endl; argv[0] is executable's full path
+	//std::cout << argc << std::endl; argc is 1
+	
+	for (int32_t i = 1; i < argc; ++i)
+	{
 
+	}
 	//------from the command argument line to get file name------
 
 	//------calculate time------
